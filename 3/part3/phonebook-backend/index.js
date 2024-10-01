0c0072b20e1aab1ab73c94af7883640cf1b275fb
zakard114 <infantry_hooa@hotmail.com>
2024-10-01 15:10:54 +1000

    Complete exercise 3.2: phonebook backend info page

--- 3/part3/phonebook-backend/index.js
+++ 3/part3/phonebook-backend/index.js
@@ -28,6 +28,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const currentTime = new Date().toString()
+  const numOfPersons = persons.length
+
+  response.send(`
+    <p>Phonebook has info for ${numOfPersons} people</p>
+    <p>${currentTime}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
